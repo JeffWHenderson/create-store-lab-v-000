14c738f577ea42780a04586dbf812c27d91c96e9
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-02 04:48:02 +0000

    automatically backed up by learn

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -6,10 +6,8 @@ action){
   switch(action.type){
 
     case 'ADD_USER':
-      console.log("hello World")
       return {...state, users: state.users.concat(action.user)}
     default:
-      console.log("default world")
       return state
   }
 }
