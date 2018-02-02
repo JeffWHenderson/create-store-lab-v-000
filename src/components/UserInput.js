bf2d79d0420b5e923739d69109c2e7150aeed7ef
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-02 04:36:02 +0000

    automatically backed up by learn

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      hometown: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
   render() {
     return(
       <div>
